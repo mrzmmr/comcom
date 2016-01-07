9245222c4ae7d8401d2b30f35c460efd1e872a06
mrzmmr <mrzmmr@gmail.com>
2016-01-07 08:47:38 +0000

    1.0.0

---
+++ src/lib/index.js
@@ -0,0 +1,30 @@
+/**
+ * TODO: Description
+ *
+ * # install
+ *
+ * ```
+ * npm install -g comcom
+ *
+ * // Or
+ *
+ * npm install --save comcom
+ * ```
+ *
+ * @module comcom
+ * @version 1.0.0
+ * @author mrzmmr
+ */
+
+import {through} from 'through'
+import {defop} from 'defop'
+
+export function format(options) {
+  options = defop(options)
+
+  return through(function (chunk) {
+    chunk = chunk.toString().split('\n').forEach((line) => {
+      this.queue(line)
+    })
+  })
+}
