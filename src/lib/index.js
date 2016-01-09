f2b61abc5973890939a2e12efda6ae842a55a846
Paul Zimmer <mrzmmr@gmail.com>
2016-01-09 13:45:32 -0800

    ...

--- src/lib/index.js
+++ src/lib/index.js
@@ -22,6 +22,23 @@
 import {through} from 'through'
 import {defop} from 'defop'
 
+/**
+ * @name options
+ * @property {String} from - Defaults to null
+ * @property {String} to - Defaults to null
+ */
+let options = {
+  from: null,
+  to: null
+}
+
+/*
+ * Buffer
+ */
+let buffer = []
+
+let switched = null
+
 /*
  * C style single line comment
  */
@@ -33,12 +50,31 @@ export const CSTYLE_SINGLE = /\s*(?=\/)\/(?=\/)\//g
 export const CSTYLE_MULTIPLE_BEG = /\s*(?=\/)\/(?=\*)\**/g
 export const CSTYLE_MULTIPLE_END = /\s*(?=\*)\**(?=\/)\//g
 
-export function split(options) {
-  options = defop(options)
+export function split(ops) {
+  ops = defop(ops)
 
   return through(function (chunk) {
     chunk = chunk.toString().split('\n').forEach((line) => {
       this.queue(line)
     })
+  })
+}
+
+export function from(ops) {
+  ops = defop(ops)
+
+  let from = ops.from
+
+  return through(function (chunk) {
+
+    /*
+     * C style multiple line
+     */
+    if (from === '/*') {
+      if (switched) {
+        buffer.push(chunk.replace(CSTYLE_MULTIPLE_MID, ' '))
+        return this.queue('null')
+      }
+    }
   })
 }
