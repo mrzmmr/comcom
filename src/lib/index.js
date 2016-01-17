09a84e59202848caa8f9329b3ae7f8685904aa5b
Paul Zimmer <mrzmmr@gmail.com>
2016-01-17 03:05:13 -0800

    bump

--- src/lib/index.js
+++ src/lib/index.js
@@ -31,7 +31,7 @@ export function split() {
     let self = this
 
     return chunk.toString().split('\n').map((line) => {
-      self.queue(line + '\n')
+      return self.queue(line + '\n')
     })
   })
 }
@@ -40,16 +40,16 @@ export function from(options, config) {
   return through(function (chunk) {
     if (options.type === 'multi') {
       if (!switched) {
-        if (chunk.startsWith(chunk.match(config[options.class].multi.begin.match))) {
-          if (!chunk.endsWith(chunk.match(config[options.class].multi.begin.match))) {
+        if (chunk.startsWith(chunk.match(config[options.class].multi.begin.match)[0])) {
+          if (!chunk.endsWith(chunk.match(config[options.class].multi.begin.match)[0])) {
             buffer.push(chunk)
           }
           return switched = true
         }
       }
       else if (switched) {
-        if (chunk.endsWith(chunk.match(config[options.class].multi.end.match))) {
-          if (!chunk.startsWith(chunk.match(config[options.class].multi.end.match))) {
+        if (chunk.endsWith(chunk.match(config[options.class].multi.end.match)[0])) {
+          if (!chunk.startsWith(chunk.match(config[options.class].multi.end.match)[0])) {
             buffer.push(chunk)
           }
           return switched = false
@@ -60,7 +60,7 @@ export function from(options, config) {
       }
     }
     else if (options.type === 'single') {
-      if (chunk.startsWith(chunk.match(config[options.class].single.match))) {
+      if (chunk.startsWith(chunk.match(config[options.class].single.match)[0])) {
         return buffer.push(chunk)
       }
     }
